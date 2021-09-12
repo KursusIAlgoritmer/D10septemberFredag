3dfd5b8d48ac3f30bb6e59e81e2dd33c315ae03a
ajrp <ajrp@tec.dk>
2021-09-12 11:07:24 +0200

    Tildes

---
+++ Tildes.java
@@ -0,0 +1,96 @@
+//1: Prøvede først med "quick-find" og et gennemløb af arrayet for hver gang size skulle beregnes
+// TIME LIMIT EXCEEDED
+
+//2: Forsøgte med "quick-find" og et "size-array" - så jeg undgik et gennemløb ...
+// WRONG ANSWER
+
+//2.1: Fejl rettede til newSize = size[i-1] + size[]
+// TIME LIMIT EXCEEDED
+
+//3: forsøger med "weighted quick union"....
+
+import java.util.Scanner;
+
+
+public class Tildes{
+    static int[] sizes; 
+    static int[] guests;
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        int N = s.nextInt();
+        int q = s.nextInt();
+
+        guests = new int[N];
+        sizes  = new int[N];
+
+        //create guest's array
+        for(int i=0; i < N ; i++){
+            guests[i] = i;
+            sizes[i] = 1;
+        }
+
+        //skifter til næste linje efter de to input tal N og q
+        String input = s.nextLine();
+
+        //read all queries
+        for(int i=0; i < q; i++){
+            input = s.nextLine();
+            String[] split = input.split(" ");
+            
+            if(split[0].equals("t")) 
+                union(Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+            if(split[0].equals("s")) 
+                System.out.println(size(Integer.parseInt(split[1])));
+
+        }
+        s.close();
+    }
+
+    public static int size(int i){
+        /*int id = find(i);
+        int s = 0;
+        for(int k : guests) if(id==k)s=s+1;
+        return s;
+        */
+        return sizes[i-1];
+    }
+
+
+    public static int find(int i){
+        return guests[i-1];
+    }
+
+    public static void union(int i, int j){
+        int idI     = find(i);
+        int idJ     = find(j);
+        int newSize   = sizes[i-1] + sizes[j-1];
+
+       // sizes[i-1] = sizes[i-1] + 1;
+       // sizes[j-1] = sizes[j-1] + 1;
+       // System.out.println(" union: " + i + " , " + j);
+
+        if(idJ != idI){
+            for(int k=0; k < guests.length; k++){
+                if(guests[k]==idJ || guests[k]==idI){
+                    guests[k]= idI;
+                    sizes[k] = newSize;
+                    //System.out.println(" ændrer nr. k til size = " + newSize);
+                }
+
+            }
+        }
+
+        /*
+        for(int m : sizes){
+            System.out.print( m + " ");
+        }
+        
+        System.out.println();
+        */
+    }
+
+
+
+}
