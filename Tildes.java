74c27acbc8bbbb07cab25c9c8445537e6c9fb088
ajrp2008 <40459685+ajrp2008@users.noreply.github.com>
2021-09-13 15:48:50 +0200

    ...

--- Tildes.java
+++ Tildes.java
@@ -13,27 +13,25 @@
 //4:Dropper "split" af string's
 // TIME LIMIT EXCEEDED
 
-//5:
+//5: https://stackoverflow.com/questions/7049011/whats-the-fastest-way-to-read-from-system-in-in-java
 import java.util.Scanner;
 
 
 public class Tildes{
     static int[] sizes;
     static int[] guests;
+
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-        int N = s.nextInt();
-        int q = s.nextInt();
+        int N = s.nextInt(); int q = s.nextInt();
 
-        guests = new int[N];
-        sizes  = new int[N];
+        guests = new int[N]; sizes  = new int[N];
 
         //create guest's array
         for(int i=0; i < N ; i++){
-            guests[i] = i;
-            sizes[i] = 1;
+            guests[i] = i; sizes[i] = 1;
         }
 
         //skifter til næste linje efter de to input tal N og q
@@ -41,54 +39,26 @@ public class Tildes{
 
         //read all queries
         for(int i=0; i < q; i++){
-            //input = s.nextLine();
             char op = s.next().charAt(0);
-            //int s1 = s.nextInt();
-            //int s2 = s.nextInt();
-            //String[] split = input.split(" ");
 
-            if(op == 't')//s.next()split[0].equals("t"))
-                union(s.nextInt(),s.nextInt());//Integer.parseInt(split[1]), Integer.parseInt(split[2]));
-            if(op == 's')//split[0].equals("s"))
-                System.out.println(size(s.nextInt()));//size(Integer.parseInt(split[1])));
+            if(op == 't') union(s.nextInt(),s.nextInt());
+            else if(op == 's') System.out.println(size(s.nextInt()));
 
         }
         s.close();
     }
 
     public static int size(int i){
-        /*int id = find(i);
-        int s = 0;
-        for(int k : guests) if(id==k)s=s+1;
-        return s;
-        */
         return sizes[find(i)];
     }
 
-
     public static int find(int i){
-        //return guests[i-1];
         i--;
-
         while(i !=  guests[i]) i = guests[i];
-
         return i;
     }
 
     public static void union(int i, int j){
-        /*int idI     = find(i);
-        int idJ     = find(j);
-        int newSize   = sizes[i-1] + sizes[j-1];
-
-        if(idJ != idI){
-            for(int k=0; k < guests.length; k++){
-                if(guests[k]==idJ || guests[k]==idI){
-                    guests[k]= idI;
-                    sizes[k] = newSize;
-                }
-
-            }
-        }*/
         int idI = find(i);
         int idJ = find(j);
         if(idI == idJ) return;
@@ -96,6 +66,4 @@ public class Tildes{
         else                        { guests[idJ] = idI ; sizes[idI] += sizes[idJ]; }
 
     }
-
-
 }
